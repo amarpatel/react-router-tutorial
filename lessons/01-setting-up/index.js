5e94bb14b04e3fd34d71218d6ed4c3d493eb085c
Amar Patel <theamarpatel@gmail.com>
2016-06-07 09:16:17 -0700

    Completed tutorial

--- lessons/01-setting-up/index.js
+++ lessons/01-setting-up/index.js
@@ -1,4 +1,40 @@
+// import React from 'react'
+// import { render } from 'react-dom'
+// import App from './modules/App'
+
+// // import { Router, Route, hashHistory, IndexRoute } from 'react-router'
+// import { Router, Route, browserHistory, IndexRoute } from 'react-router'
+// import About from './modules/About'
+// import Repos from './modules/Repos'
+// import Repo from './modules/Repo'
+// import Home from './modules/Home'
+
+
+// // IndexRoute becomes this.props.children of the parent when no other child of the parent matches, or in other words, when the parent's route matches exactly
+
+// render((
+//     <Router history={browserHistory}>
+//         <Route path="/" component={App}>
+//             <IndexRoute component={Home} />
+//             <Route path="/about" component={About} />
+//             <Route path="/repos" component={Repos}>
+//                 <Route path="/repos/:userName/:repoName" component={Repo} />
+//             </Route>
+//         </Route>
+//     </Router>
+// ), document.getElementById('app'))
+
+
+// ^before server
+// after server: 
+
 import React from 'react'
 import { render } from 'react-dom'
-import App from './modules/App'
-render(<App/>, document.getElementById('app'))
+import { Router, browserHistory } from 'react-router'
+// import routes and pass them into <Router/>
+import routes from './modules/routes'
+
+render(
+  <Router routes={routes} history={browserHistory}/>,
+  document.getElementById('app')
+)
--- lessons/01-setting-up/modules/App.js
+++ lessons/01-setting-up/modules/App.js
@@ -1,7 +1,40 @@
 import React from 'react'
+import NavLink from './NavLink'
+import Home from './Home'
+import { IndexLink } from 'react-router'
 
 export default React.createClass({
-  render() {
-    return <div>Hello, React Router!</div>
-  }
+    render() {
+        // return (
+            // <div>{this.props.children || <Home/>}</div>
+        // )
+        return (
+            <div style={{ backgroundColor: 'lightgrey' }}>
+                <h1>React Router Tutorial</h1>
+                <ul role="nav">
+                    <li>
+                        <NavLink to="/" onlyActiveOnIndex={true}>Home</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/about">About</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/repos">Repos</NavLink>
+                    </li>
+                </ul>
+                {this.props.children}
+            </div>
+        )
+    }
 })
+
+
+/*
+
+<IndexLink to="/" activeClassName="active">Home</IndexLink>
+can also be
+<Link to="/" activeClassName="active" onlyActiveOnIndex={true}>Home</Link>
+but we're using spread (like this: <Link {...this.props} > ...etc)
+so we can do it the above way
+
+*/
